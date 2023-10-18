abf476068459e2834d256fa02795fb99dd0d0b68
ToniPortal <pastre.toni@gmail.com>
2023-10-18 14:04:23 +0200

    first commit

---
+++ exoplus.js
@@ -0,0 +1,38 @@
+function getElement(selector) {
+    return document.querySelectorAll(selector);
+}
+
+function setAttribute(selector, name, value) {
+    getElement(selector).forEach(element => {
+        element.setAttribute(name, value)
+    });
+}
+
+async function getAttribute(selector, name) {
+    let all = [];
+    getElement(selector).forEach(async element => {
+        var get = element.getAttribute(name);
+        if (get !== null) {
+            all.push(get)
+        }
+    });
+    return all;
+}
+
+async function addAttribute(selector, name, value) {
+    var all = await getAttribute(selector,name)
+    console.log(all.join(","))
+    getElement(selector).forEach(el => {
+        el.setAttribute(name, all.join(", ") + value)
+    })
+}
+
+window.onload = async function () {
+    //console.log(getElement("td"))
+
+    // setAttribute("td", "class", "note");
+
+    // console.log(await getAttribute("td", "class"));
+
+    addAttribute("td", "class", "note")
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    //var n = Number(prompt("entre n"));
+    // document.getElementById("rep").innerText = ((n*(n+1)) / 2);
+    
+   // document.getElementsByTagName("td")[0].classList.add("note")
+
+    console.log(document.getElementsByTagName("td")[0])
+    
+}
